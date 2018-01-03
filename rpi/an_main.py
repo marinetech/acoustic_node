daab6595161ee332a7fb651d092ace766af7395f
Ilan Mardix <ilan.mardix@gmail.com>
2018-01-03 13:58:48 +0200

    ongoing

--- rpi/an_main.py
+++ rpi/an_main.py
@@ -12,7 +12,7 @@ if  __name__ == "__main__":
     except Exception as e:
         self.log.print_log("-E- " + e)
         exit(1)
-        
+
     net.connect_func(pc104_ip,pc104_user,pc104_user_passwd)
     net.get_func(pc104_tasks_folder , rpi_tasks_folder)
     net.put_func(rpi_done_folder, pc104_done_folder)
