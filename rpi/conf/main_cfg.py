ea166fceb4cc8bb22cc0aee999ba376e7f435702
Ilan Mardix <ilan.mardix@gmail.com>
2018-01-04 13:58:20 +0200

    on going

---
+++ rpi/conf/main_cfg.py
@@ -0,0 +1,27 @@
+import datetime
+
+# general
+now = datetime.datetime.now().strftime("%Y%m%d%H%M")
+net_protocol = "ssh"
+
+# pc104 related settings
+pc104_ip = "127.0.0.1"
+pc104_ip = "192.168.0.100"
+pc104_tasks = "/dmz/tasks"
+pc104_done = "/dmz/done"
+pc104_user = "pi"
+pc104_user_passwd = "raspberry"
+
+
+# rpi file system
+rpi_root = "/home/pi/acoustic_node"
+rpi_tasks = rpi_root + "/tasks"
+rpi_done = rpi_root + "/done"
+rpi_logs = rpi_root + "/logs"
+rpi_bin = rpi_root + "/bin"
+log_path = rpi_logs + "/main_" + now + ".log"
+
+
+# modem related settings
+modem_ip = "192.168.0.149"
+modem_port = "9200"
---
+++ rpi/utils/mkdirs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p acoustic_node/logs
+mkdir -p /home/pi/acoustic_node/tasks
+mkdir -p /home/pi/acoustic_node/done
+
+sudo apt-get install python3-pip
+sudo pip3 install paramiko
+#? sudo apt-get install openssh-server
+sudo apt-get install rpi.gpio
