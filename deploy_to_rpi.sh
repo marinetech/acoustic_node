cbc2caeb5492ed8f2f01368d01336220de44bc46
Ilan Mardix <ilan.mardix@gmail.com>
2018-03-26 14:04:07 +0300

    wq

---
+++ deploy_to_rpi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+src_dir="/home/ilan/projects/acoustic_node/rpi"
+dply_dir="/home/ilan/projects/acoustic_node/rpi_dply"
+
+cp -r $src_dir $dply_dir
---
+++ rpi_dply/an_main.py
@@ -0,0 +1,29 @@
+# import datetime
+from lib.network import *
+from lib.log import *
+from lib.gpio import *
+from lib.sdm import *
+from conf.main_cfg import *
+
+
+if  __name__ == "__main__":
+    print("")
+    print("*** Starting the main loop of the acoustic-node operation ***")
+    print("")
+    try:
+        log = Log(log_path)
+        net = Network(net_protocol, log)
+        gpio = CtrlGPIO(log)
+        sdm = SDM(modem_ip, modem_port, rpi_tasks, rpi_bin, log)
+    except Exception as e:
+        print("-E- " + str(e))
+        exit(1)
+
+
+    gpio.set_comm_mode() # pwr-on rpi + dsl
+    net.connect_func(pc104_ip,pc104_user,pc104_user_passwd) # establish ssh connection with pc104
+    net.get_func(pc104_tasks , rpi_tasks) # get new tasks
+    gpio.set_operation_mode # pwr-on rpi + modem
+    sdm.process_tasks() # run SDM tasks
+    gpio.set_comm_mode() # pwr-on rpi + dsl
+    net.put_func(rpi_done, pc104_done) # upload results
